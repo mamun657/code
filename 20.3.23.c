4640a011de3abb6620670294e20cf67a79ebbc48
Minul Islam Mamun <128472207+mamun657@users.noreply.github.com>
2023-04-17 21:42:54 +0600

    Add files via upload

---
+++ 20.3.23.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    int c;
+    scanf("%d%d",&a,&b);
+  if(a>b)
+  {
+      c=a;
+  }
+  else
+  {
+      c=b;
+  }
+  printf("%d",c);
+    return 0;
+}
